e8c965f518e4c68bb5cbcfab896c5f765fe8a2ca
manoz-manoz <examhubfeedback@gmail.com>
2025-03-21 12:16:07 +0530

    Add missing folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-<BrowserRouter >
-    <App />
-    </BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <BrowserRouter  basename="/react-frontend-restaurant">
+        <App />
+      </BrowserRouter>
+    </StrictMode>
 
 )
 window.addEventListener('load', () => {
